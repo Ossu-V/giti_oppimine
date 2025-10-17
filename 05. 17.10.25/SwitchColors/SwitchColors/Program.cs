279dbc67e6a07ad47f2c59edacadd1345801df39
Ossu-V <oscarvalk7@gmail.com>
2025-10-17 11:44:34 +0300

    SwitchColor

---
+++ 05. 17.10.25/SwitchColors/SwitchColors/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Drawing;
+
+namespace SwitchColors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            
+            Color color = (Color)(new Random()).Next(0, 5);
+
+            switch (color)
+            {
+                case Color.Red:
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.Clear();
+                    Console.WriteLine("Red");
+                    break;
+                case Color.Green:
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.Clear();
+                    Console.WriteLine("Green");
+                    break;
+                case Color.Blue:
+                    Console.BackgroundColor = ConsoleColor.Blue;
+                    Console.Clear();
+                    Console.WriteLine("Blue");
+                    break;
+                case Color.White:
+                    Console.BackgroundColor = ConsoleColor.White;
+                    Console.Clear();
+                    Console.WriteLine("White");
+                    break;
+                case Color.Orange:
+                    Console.BackgroundColor = ConsoleColor.Yellow;
+                    Console.Clear();
+                    Console.WriteLine("Orange");
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public enum Color
+        {
+            Red,
+            Green,
+            Blue,
+            White,
+            Orange
+        }
+    }
+}
