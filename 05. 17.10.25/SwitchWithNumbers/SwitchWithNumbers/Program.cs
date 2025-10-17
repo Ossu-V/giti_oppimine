8acc0973c58beb3cee4d3275492710bb084bdbca
Ossu-V <oscarvalk7@gmail.com>
2025-10-17 11:16:36 +0300

    SwitchWithNumbers

---
+++ 05. 17.10.25/SwitchWithNumbers/SwitchWithNumbers/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace SwitchWithNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            int intput = int.Parse(Console.ReadLine());
+
+            switch (intput)
+            {
+                case 1:
+                    Console.Beep();
+                    break;
+
+                case 2:
+                    Console.Beep();
+                    Console.Beep();
+                    break;
+
+                case 3:
+                    Console.Beep();
+                    Console.Beep();
+                    Console.Beep();
+                    break;
+
+                default:
+                    Console.WriteLine("ERROR");
+                    break;
+            }
+        }
+    }
+}
