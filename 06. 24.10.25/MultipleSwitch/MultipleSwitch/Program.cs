d39b294070a3dcb44a1f7a715e8cadec8fb31b32
Opilane TTHK <oscarvalk7@gmail.com>
2025-10-24 11:42:53 +0300

    MultipleSwitch

---
+++ 06. 24.10.25/MultipleSwitch/MultipleSwitch/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace MultipleSwitch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Multiple cases");
+
+            string topic, category;
+
+            topic = Console.ReadLine();
+
+            switch (topic)
+            {
+                case "Intro to c#":
+                case "Variables":
+                case "Data Types":
+                    category = "Basics";
+                    break;
+
+                case "Loops":
+                case "If statements":
+                case "Jump statements":
+                    category = "Control Flow";
+                    break;
+
+                case "Class & Objects":
+                case "Inheritance":
+                case "Construction":
+                    category = "OOP-s Concept";
+                    break;
+
+                default:
+                    category = "unknown";
+                    break;
+            }
+            Console.WriteLine("Category is " + category);
+        }
+    }
+}
