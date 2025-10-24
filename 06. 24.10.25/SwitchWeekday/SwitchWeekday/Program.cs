9ea90e28c20c06591f71720988c69fb83414d229
Opilane TTHK <oscarvalk7@gmail.com>
2025-10-24 11:29:45 +0300

    SwitchWeekday

---
+++ 06. 24.10.25/SwitchWeekday/SwitchWeekday/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace SwitchWeekday
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            //tuleb kasutada switch lauset
+            //kui nädalapäev on esmaspäev, siis prinditakse "Tere esmaspäev"
+            //peate kasutama DayOfWeek enumeratsiooni
+
+            switch (DateTime.Now.DayOfWeek)
+            {
+                case DayOfWeek.Monday:
+                    Console.WriteLine("Tere esmaspäev");
+                    break;
+
+                case DayOfWeek.Tuesday:
+                    Console.WriteLine("Tere teisipäev");
+                    break;
+
+                case DayOfWeek.Wednesday:
+                    Console.WriteLine("Tere kolmapäev");
+                    break;
+
+                case DayOfWeek.Thursday:
+                    Console.WriteLine("Tere neljapäev");
+                    break;
+
+                case DayOfWeek.Friday:
+                    Console.WriteLine("Tere reede");
+                    break;
+
+                case DayOfWeek.Saturday:
+                    Console.WriteLine("Tere laupäev");
+                    break;
+
+                case DayOfWeek.Sunday:
+                    Console.WriteLine("Tere pühapäev");
+                    break;
+
+                default:
+                    Console.WriteLine("ERROR");
+                    break;
+            }
+                       
+        }
+    }
+}
