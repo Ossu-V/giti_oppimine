f7a266d93ddf1e1816312af2383760dedef5f430
Ossu-V <oscarvalk7@gmail.com>
2025-10-31 09:43:33 +0200

    SwitchMultipleCases

---
+++ 07. 31.10.25/SwitchMultipleCases/SwitchMultipleCases/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace SwitchMultipleCases
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            //sisestad konsooli väärtuse
+            //loeb sisestatud väärtuse stringina
+            //tuleb kasutada switchi
+            //esimeses cases on väärtused "intro to C#", "Variables",
+            //"Data Types" ja vastuseks Basic
+            //teises cases on väärtused "OOP", "Classes",
+            //"Objects" ja vatuseks Intermediate
+            //kolmandas cases on väärtused "Asynchronous Programming", "LINQ",
+            //"Delegates" ja vastuseks Advanced
+
+            string topic, category;
+
+            topic = Console.ReadLine();
+
+
+            switch (topic)
+
+            {
+                case "intro to C#":
+                case "Variables":
+                case "Data Types":
+                    category = "Basic";
+                    break;
+
+                case "OOP":
+                case "Classes":
+                case "Objects":
+                    category = "Intermediate";
+                    break;
+
+                case "Asynchronous Programming":
+                case "LINQ":
+                case "Delegates":
+                    category = "Advanced";
+                    break;
+            }
+        }
+    }
+}
