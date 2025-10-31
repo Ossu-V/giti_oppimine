e0306e2917eb1549c0146a83a4bb23aa4fa84c17
Ossu-V <oscarvalk7@gmail.com>
2025-10-31 10:49:15 +0200

    SwitchMethodCall

---
+++ 07. 31.10.25/SwitchMethodCall/SwitchMethodCall/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace SwitchMethodCall
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            //teha switchi rakendus,
+            //kus kasutaja sisestab sõiduki tüübi (coipe või truck)
+            //ja programm kutsub vastava meetodi (CarCoupe või Truck)
+
+            string car = Console.ReadLine();
+
+            switch (car)
+            {
+                case "CarCoupe":
+                    CarCoupe();
+                    break;
+
+                case "Truck":
+                    Truck();
+                    break;
+
+                default:
+                    Console.WriteLine("ERROR");
+                    break;
+            }
+
+        }
+
+        static void CarCoupe()
+        {
+            Console.WriteLine("This is a coupe car.");
+        }
+
+        static void Truck()
+        {
+            Console.WriteLine("This is a truck");
+        }
+    }
+}
