f7e47d710b516acc28069fd39653bee4c2395828
Opilane TTHK <oscarvalk7@gmail.com>
2025-11-14 09:32:48 +0200

    SwitchCalculator

---
+++ 08. 14.11.25/SwitchCalculator/SwitchCalculator/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace SwitchCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            //loeme kasutaja sisendit ja teisendame selle float tüübiks
+            float result = 0;
+
+            Console.WriteLine("Sisesta esimene number");
+            float num1 = float.Parse(Console.ReadLine());
+
+            //konsool kirjutab kasutajale , et vali tehe: +, -, *, /
+
+            Console.WriteLine("Sisesta tehe (+, -, *, /");
+            string operation = Console.ReadLine();
+
+            //konsool kirjutab, et sisesta teine number ja teisendame selle float tüübiks
+
+            Console.WriteLine("Sisesta teine number");
+            float num2 = float.Parse(Console.ReadLine());
+
+            switch (operation)
+
+            {
+                case "+":
+                    result = num1 + num2;
+                    break;
+                case "-":
+                    result = num1 - num2;
+                    break;
+                case "*":
+                    result = num1 * num2;
+                    break;
+                
+
+
+            }
+            Console.WriteLine("Vastus = " + result);
+            switch (operation)
+            {
+                case "/":
+
+                    if (num1 == 0)
+                    {
+                        Console.WriteLine("Nulliga ei saa jagada ERROR");
+                    }
+                    else if (num2 == 0)
+                    {
+                        Console.WriteLine("Nulliga ei saa jagada");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Vastus = ERROR");
+                    }
+                    result = num1 / num2;
+                    break;
+            }
+        }
+    }
+}
