e458dd17554635bfdf50fd392a64dd46f16dd7aa
Opilane TTHK <oscarvalk7@gmail.com>
2025-11-21 09:40:23 +0200

    update.error

--- 09. 21.11.25/TryCatchSwitch/TryCatchSwitch/Program.cs
+++ 09. 21.11.25/TryCatchSwitch/TryCatchSwitch/Program.cs
@@ -4,9 +4,34 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Try catch Switch");
 
+            //tee switch, kus on ainult kaks case´i
+            //ja valikus on 1 ja 2
+            //default haru ei ole vajalik
+            try
+            {
 
+                int nr = int.Parse(Console.ReadLine());
+
+                switch (nr)
+                {
+                    case 1:
+                        Console.WriteLine("Valisid 1");
+                        break;
+                    case 2:
+                        Console.WriteLine("Valisid 2");
+                        break;
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Palun sisesta number!");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Sisestatud number on liiga suur või liiga väike!");
+            }
         }
     }
 }
