ad71bc250c855c4b6a2a61168b20dec16302a215
Opilane TTHK <oscarvalk7@gmail.com>
2025-11-21 09:06:20 +0200

    TryCatchSwitch

---
+++ 09. 21.11.25/TryCatchSwitch/TryCatchSwitch/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TryCatchSwitch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
