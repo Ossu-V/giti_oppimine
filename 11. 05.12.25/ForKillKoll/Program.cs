607e71773a7ddbfee882741579f1eba0d3ff0f4a
Ossu-V <oscarvalk7@gmail.com>
2025-12-05 11:42:53 +0200

    ForKillKoll

---
+++ 11. 05.12.25/ForKillKoll/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace ForKillKoll
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Elenevaid teadmisi kasutades kirjuta programm, mis väljastaks:
+            //kill - koll
+            //kill - koll
+            //killadi - koll
+            //kill - koll
+            //kill - koll
+            //killadi - koll
+            //kill - koll
+            //kill - koll
+            //killadi - koll
+            //kill - koll
+
+            Console.WriteLine("Sisesta korduste arv");
+
+            int arv = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < arv; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    Console.WriteLine("kill - koll");
+                }
+
+                for (int k = 0; k < 1; k++)
+                {
+                    Console.WriteLine("killadi - koll");
+                }
+            }
+            Console.WriteLine("kill - koll");
+        }
+    }
+}
