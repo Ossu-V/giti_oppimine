03ea7cac183e6c0a141d38a06dff74a9244a5fc2
Opilane TTHK <oscarvalk7@gmail.com>
2026-01-05 09:37:03 +0200

    ForLoopArray

--- 15. 05.01.26/ForLoopArray/Program.cs
+++ 15. 05.01.26/ForLoopArray/Program.cs
@@ -5,6 +5,16 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            //vaja teha string array H, e, l, l, o
+            //kasutada for loopi
+
+            string[] array = { "H", "e", "l", "l", "o" };
+
+            for (int i = 0; i < array.Length ; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
         }
     }
 }
