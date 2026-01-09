e1260f147f9370b94934df9a69033b8b896ed8f2
Opilane TTHK <oscarvalk7@gmail.com>
2026-01-09 11:05:50 +0200

    Ruut

---
+++ 16. 09.01.26/Ruut/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics.Metrics;
+using System.Numerics;
+
+namespace Ruut
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta ruudu suurus:");
+            //teha for loopiga ruut
+            int size = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            for (int i = 0; i < size; i++)
+            {
+                for (global::System.Int32 j = 0; j < size; j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            
+            //kui suurus on sisestatud, siis kuvab meile ka ümbermõõdu ja pindala
+
+            int area = size * size;
+            int around = 4 * size;
+
+            Console.WriteLine("Ruudu pindala on: " + area);
+            Console.WriteLine("Ruudu ümbermõõt on: " + around);
+        }
+    }
+}
